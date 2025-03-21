7c507af07e539da2e9f53e50327bd61792a8fc87
Aby George <abygeorge133@gmail.com>
2025-03-21 09:39:59 +0530

    Add role based authorization functionality

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { getMovies,getOneMovie,addMovie,deleteMovie } from '../controllers/movieController.js'
 import verifyToken from '../middlewares/authMiddleware.js'
-
+import authorizeRole from '../middlewares/roleMiddleware.js'
 
 const router = express.Router()
 
@@ -9,13 +9,13 @@ const router = express.Router()
 
 
 
-router.post('/movie',verifyToken, addMovie)
-router.delete('/movie/:id',verifyToken, deleteMovie)
+router.post('/movie',verifyToken, authorizeRole("admin"), addMovie)
+router.delete('/movie/:id',verifyToken, authorizeRole("admin"), deleteMovie)
 
 
 
 
-router.get('/movies', verifyToken, getMovies)
-router.get('/movie/:id', verifyToken, getOneMovie)
+router.get('/movies', verifyToken, authorizeRole("admin","user"), getMovies)
+router.get('/movie/:id', verifyToken, authorizeRole("admin","user"), getOneMovie)
 
 export default router
